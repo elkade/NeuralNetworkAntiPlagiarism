20fb6e2801b929a1ed88cf73d889dfa483ab333b
Łukasz <draganl@student.mi.pw.edu.pl>
2017-01-10 00:55:28 +0100

    operacje na CSVce

--- NeuralNetworkAntiPlagiarism/InputDataReader.py
+++ NeuralNetworkAntiPlagiarism/InputDataReader.py
@@ -1,58 +1,72 @@
 from xml.etree import ElementTree
 import pickle
 import threading
+import csv
+import ast
 
 class InputDataReader(object):
     def __init__(self, processor):
         self.processor = processor
         self.lock = threading.Lock()
-    def run(self, path):
-        print("start: "+path)
-        fp = open(path + ".txt", 'r', encoding='utf-8')
-        text = fp.read()
-        fp.close()
+    def run(self, path, output_path, serialize):
+        try:
+            print("start: "+path)
+            fp = open(path + ".txt", 'r', encoding='utf-8')
+            text = fp.read()
+            fp.close()
 
-        xmlRoot = ElementTree.parse(path + ".xml").getroot()
-        metadata = []
-        nodes = [child for child in xmlRoot if child.attrib['name'] == 'plagiarism']
-        for node in nodes:
-            metadata.append(node.attrib)
+            xmlRoot = ElementTree.parse(path + ".xml").getroot()
+            metadata = []
+            nodes = [child for child in xmlRoot if child.attrib['name'] == 'plagiarism']
+            for node in nodes:
+                metadata.append(node.attrib)
 
-        (x, y) = self.processor.get_input({'text':text, 'metadata': metadata})
-        self.lock.acquire()
-        self.X.extend(x)
-        self.Y.extend(y)
-        self.lock.release()
-        print("end: "+path)
+            (X, Y) = self.processor.get_input({'text':text, 'metadata': metadata})
+            self.lock.acquire()
+            #self.X.extend(x)
+            #self.Y.extend(y)
+            if serialize:
+                with open(output_path, 'a', newline='') as csvfile:
+                    writer = csv.DictWriter(csvfile, delimiter=';', quoting=csv.QUOTE_NONE, fieldnames=['x', 'y'])
+                    writer.writeheader()
+                    for i in range(0,len(X)):
+                        writer.writerow({'x':X[i], 'y':Y[i]})
+            self.lock.release()
+            print("end: "+path)
+        except Exception as ex:
+            print(ex.args)
     def read(self, partInd, startInd, endInd, serialize = True):
         self.X = []
         self.Y = []
         threads=[]
+        output_path = 'features/part{}_{}_{}.csv'.format(partInd, startInd, endInd)
         for ind in range(startInd, endInd):
             print('file index: ' + str(ind))
             path = "dataSets/part{}/suspicious-document{:05d}".format(partInd, 500 * (partInd - 1) + ind)
-            thread=threading.Thread(target=self.run, args=([path]))
-            thread.start()
+            thread=threading.Thread(target=self.run, args=([path, output_path, serialize]))
             threads.append(thread)
+            thread.start()
+            
+            #with open('features/part{}_{}_{}'.format(partInd, startInd, endInd), 'wb+') as handle:
+            #    pickle.dump({'X':self.X,'y':self.Y}, handle)
         for thread in threads:
             thread.join()
             #X.extend(reader.X)
             #y.extend(reader.Y)
             pass
-        if serialize:
-            with open('features/part{}_{}_{}'.format(partInd, startInd, endInd), 'wb+') as handle:
-                pickle.dump({'X':self.X,'y':self.Y}, handle)
-
         return (self.X, self.Y)
     def read_features(self, filename):
         X, y = [], []
+        buf = []
         try:
-            with open('features/{}'.format(filename), 'rb') as handle:
-                storedFeatures = pickle.load(handle)
-            X.extend(storedFeatures['X'])
-            y.extend(storedFeatures['y'])
-        except:
-            print('błąd odczytu pliku features/{}'.format(filename))
+            with open('features/{}'.format(filename), 'r', newline='') as csvfile:
+                reader = csv.DictReader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
+                for row in reader:
+                    y.append(float(row['y']))
+                    x = ast.literal_eval(row['x'])
+                    X.append(x)
+        except Exception as ex:
+            print('błąd odczytu pliku features/{}'.format(filename), ex.args)
 
         return X, y
 
--- NeuralNetworkAntiPlagiarism/start.py
+++ NeuralNetworkAntiPlagiarism/start.py
@@ -12,20 +12,21 @@ e = FeaturesExtractor()
 p = InputDataProcessor(a, e, (0.2, 0.8))
 r = InputDataReader(p)
 
-#(X, y) = r.read(1, 322, 322)
-(X, y) = r.read_features('part1')
+#(X, y) = r.read(1, 1, 2)
+
+(X, y) = r.read_features('part1_1_2.csv')
 #n = NetworkMock()
-n = MLPClassifier(solver='lbfgs', alpha=1e-5,  hidden_layer_sizes=(5,), random_state=1)
+#n = MLPClassifier(solver='lbfgs', alpha=1e-5,  hidden_layer_sizes=(5,), random_state=1)
 
-n.fit(X, y)
+#n.fit(X, y)
 
-a = Atomizer('test')
-e = FeaturesExtractor()
+#a = Atomizer('test')
+#e = FeaturesExtractor()
 
-t = Tester(a, e, n, 0.8)
+#t = Tester(a, e, n, 0.8)
  
-test_file = r.get_file("dataSets/part{}/suspicious-document{:05d}".format(8, 500 * (8 - 1) + 1))
-b = t.is_plagiarised(test_file)
-print('odpowiedz systemu: ' + str(b[0]))
+#test_file = r.get_file("dataSets/part{}/suspicious-document{:05d}".format(8, 500 * (8 - 1) + 1))
+#b = t.is_plagiarised(test_file)
+#print('odpowiedz systemu: ' + str(b[0]))
 
-print('stan rzeczywisty: ' + str(not not test_file['metadata']))
+#print('stan rzeczywisty: ' + str(not not test_file['metadata']))
